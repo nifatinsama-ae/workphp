e589bd28a3a1fd338f7a1a4172b1c558a2ca149a
nifatinsama-ae <64309010010@ptnicec.ac.th>
2022-10-21 14:27:23 +0700

    ส่งครั้งที่ 1

---
+++ comment.php
@@ -0,0 +1,16 @@
+<?php
+//บรรทัดที่ 6-7 แสดงผลการใช้งาน Comment แบบบรรทัดเดียว
+echo "Nifatin sama-ae<br>";
+echo "Information Technology";
+//echo "Pattani<br>";
+#echo "Thailand<br>";
+
+//บรรทัดที่ 10-12 แสดงผลการใช้งาน Comment แบบบรรทัดเดียว
+/* echo "<br>";
+echo "94000<br>";
+echo "Asia<br>";*/
+echo "Universal<br>";
+//
+
+
+?>
---
+++ test.php
@@ -0,0 +1,25 @@
+<?php
+
+    echo "Nifatin Sama-ae";
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>TEST</title>
+</head>
+<body>
+    <br> Information Technology <br>
+    <?php
+        echo "Pattani";
+    ?>
+    <br>
+</body>
+</html>
+<?php
+        echo "Thailand";
+    ?>
