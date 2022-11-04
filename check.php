7f5ee23b6bddf9d22e2f186ce0f4a41b301a72e0
nifatinsama-ae <64309010010@ptnicec.ac.th>
2022-11-04 16:38:15 +0700

    ส่งครั้งที่1

---
+++ check.php
@@ -0,0 +1,36 @@
+<?php
+
+$prefix = $_POST['prefix'];
+
+//if($prefix == 'นาย'){
+//    echo "ผู้ชาย";
+//}elseif($prefix == 'เด็กชาย'){
+//    echo "ผู้ชาย";
+//}elseif($prefix == 'นาง'){
+//   echo "ผู้หญิง";
+//}elseif($prefix == 'นางสาว'){
+//    echo "ผู้หญิง";
+//}else{
+//    echo "ผู้หญิง";
+//}
+
+switch ($prefix) {
+    case 'นาง':
+        echo "ผู้หญิง";
+        break;
+    case 'นางสาว':
+        echo "ผู้หญิง";
+            break;
+    case 'เด็กหญิง':
+        echo "ผู้หญิง";
+                break;
+    case 'นาย':
+        echo "ผู้ชาย";
+                    break;
+
+    default:
+        echo "ผู้ชาย";
+        break;
+}
+
+?>
---
+++ ifelse.php
@@ -0,0 +1,28 @@
+<?php
+
+    $score = 16;
+    if($score % 2 == 0 ){
+        echo "เลขคู่";
+    }
+    echo "<hr color='red'>";
+    
+    $led = "green";
+
+    if($score % 2 == 0){
+        echo "เลขคู่";
+    }else{
+        echo "เลขคี่";
+    }
+    echo "<hr color='red'>";
+    $led = "red";
+
+    if($led == 'red'){
+        echo "STOP!!!";
+    }elseif($led == 'yellow'){
+        echo "SLOWLY!!!";
+    }else{
+        echo "GOING!!!";
+    }
+
+
+?>
